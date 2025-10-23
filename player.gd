251f7a0f3d96213547febcd6ffe0057b9a6cd71a
Snowy <snowyyang@outlook.com>
2025-10-24 01:36:20 +0800

    v2 sketch up

--- player.gd
+++ player.gd
@@ -1,18 +1,20 @@
 extends Node2D
 
-@onready var line: Line2D = $Line
+@onready var upper_arm: Line2D = $RightUpperArm
+@onready var forearm: Line2D = $RightUpperArm/Forearm
+@onready var rod: Line2D = $RightUpperArm/Forearm/Rod
+@onready var line: Line2D = $RightUpperArm/Forearm/Rod/Line
 # @onready var bubble: Sprite2D = $Bubble
 
 var max_segment_count = 50
 var initial_velocity = 10.0
 var angle = 45.0
-var gravity = 9.8
 var max_line_length = 10.0
 var end_angle = 120.0
 
-var bait_target_position: Vector2
-var bait_position: Vector2
-var bait_position0: Vector2
+var bait_target_global_position: Vector2
+var bait_global_position: Vector2
+var bait_global_position0: Vector2
 
 var casting = false
 var rotation_speed_down = false
@@ -24,16 +26,20 @@ var parabola_time = 0.0
 var curve_offset = 0.3
 
 var local_rotation0: float
+var bait_initial_velocity: Vector2
+const g_vec = Vector2(0, 9.8) * 500
+const bait_duration = 0.35
 
 func _ready():
 	# bubble.visible = false
-	local_rotation0 = rotation_degrees
-	bait_position = global_position
-	bait_position0 = global_position
+	local_rotation0 = upper_arm.rotation_degrees
+	bait_global_position0 = line.global_position
+	bait_global_position = bait_global_position0
 	_reset()
 
 func _process(delta):
 	if Input.is_action_just_pressed("mouse_left"):
+		_reset()
 		_start_casting()
 
 	if Input.is_action_just_pressed("esc_reset"):
@@ -42,7 +48,6 @@ func _process(delta):
 	if Input.is_action_just_pressed("show_data"):
 		print("Velocity:", initial_velocity)
 		print("Angle:", angle)
-		print("Gravity:", gravity)
 		print("End Angle:", end_angle)
 
 	if casting:
@@ -53,19 +58,12 @@ func _process(delta):
 
 	if casting or bait_down:
 		_update_bait_position(delta)
-		_draw_bezier(global_position, bait_position, Vector2(1,-1), Vector2(-1,1), curve_offset)
+		_draw_bezier(line.global_position, bait_global_position, Vector2(1,-1), Vector2(-1,1), curve_offset)
 
 func _start_casting():
-	segment_increment = 0.5
-	segment_count = 0
-	initial_velocity = 10.0
-	angle = 45.0
-	gravity = 9.8
-	max_line_length = 10.0
-	end_angle = 120.0
-	bait_position0 = global_position
-	bait_position = bait_position0
-	bait_target_position = get_global_mouse_position()
+	bait_target_global_position = get_global_mouse_position()
+	# end = start + v0 * T + 0.5 * g_vec * T^2
+	bait_initial_velocity = (bait_target_global_position - bait_global_position0 - 0.5 * g_vec * bait_duration * bait_duration) / bait_duration
 	casting = true
 	parabola_time = 0.0
 	line.clear_points()
@@ -73,12 +71,11 @@ func _start_casting():
 
 func _reset():
 	# bubble.visible = false
-	rotation_degrees = local_rotation0
+	upper_arm.rotation_degrees = local_rotation0
 	line.clear_points()
 	max_segment_count = 50
 	initial_velocity = 10.0
-	angle = 45.0
-	gravity = 9.8
+	angle = 30.0
 	max_line_length = 10.0
 	end_angle = 120.0
 	casting = false
@@ -87,20 +84,21 @@ func _reset():
 	speed = 1.0
 	segment_count = 0
 	segment_increment = 0
-	bait_position = global_position
+	bait_global_position0 = line.global_position
+	bait_global_position = bait_global_position0
 	parabola_time = 0.0
 
 func _handle_casting(delta): #rod rotation
 	if not rotation_speed_down:
-		rotation_degrees -= 110 * delta * 5.0 * speed
+		upper_arm.rotation_degrees -= 110 * delta * 5.0 * speed
 		speed *= 1.01
 		curve_offset *= (1 - 0.05 * delta)
-		if rotation_degrees < -60:
+		if upper_arm.rotation_degrees < 120:
 			rotation_speed_down = true
 	else:
-		rotation_degrees -= 10 * delta * speed
+		upper_arm.rotation_degrees -= 10 * delta * speed
 		speed *= 0.98
-		if rotation_degrees < -75:
+		if upper_arm.rotation_degrees < 105:
 			casting = false
 			bait_down = true
 			curve_offset = 0
@@ -110,24 +108,21 @@ func _handle_bait_fall(delta):
 	if curve_offset > -0.15:
 		curve_offset -= 1.0 * delta
 
-func _update_bait_position(delta): #parabola
-	parabola_time += delta * 3
-	var start = bait_position0
-	var end = bait_target_position
-	var duration = 0.3
-	var g = 9.8 * 30
+# compute unique physical initial velocity v0 such that:
+func _update_bait_position(delta): #parabola (physical)
+	parabola_time += delta
+	var start = bait_global_position0
+	var duration = bait_duration
+	if duration <= 0:
+		return
 
-	var velocity = Vector2(end.x - start.x, 0) * 1
-	var height_offset = 45
-	var vy = (height_offset + 0.5 * g * duration * duration) * 3
+	# clamp time and compute physical position
+	var t = clamp(parabola_time, 0.0, duration)
+	var pos = start + bait_initial_velocity * t + 0.5 * g_vec * t * t
+	bait_global_position = pos
 
-	var t = parabola_time
-	var pos = start + velocity * t
-	pos.y -= vy * t - 0.5 * g * t * t
-
-	bait_position = pos
-
-	if bait_position.y > bait_target_position.y:
+	# if bait passed below target, stop
+	if bait_global_position.y > bait_target_global_position.y:
 		casting = false
 		bait_down = false
 		# bubble.global_position = bait_position
@@ -137,7 +132,7 @@ func _update_bait_position(delta): #parabola
 func _draw_bezier(start: Vector2, end: Vector2, control1: Vector2, control2: Vector2, offset: float):
 	var dir = (end - start).normalized()
 	var normal = Vector2(-dir.y, dir.x).normalized()
-	offset *= 15
+	offset *= (end - start).length() / 10
 	control1 = start + dir * ((start.distance_to(end)) / 3) + normal * offset
 	control2 = start + dir * ((start.distance_to(end)) * 2 / 3) - normal * offset
 
@@ -146,4 +141,4 @@ func _draw_bezier(start: Vector2, end: Vector2, control1: Vector2, control2: Vec
 	for i in range(segment_count):
 		var t = float(i) / segment_count
 		var point = (1 - t) ** 3 * start + 3 * (1 - t) ** 2 * t * control1 + 3 * (1 - t) * t ** 2 * control2 + t ** 3 * end
-		line.add_point(to_local(point))
+		line.add_point(line.to_local(point))
